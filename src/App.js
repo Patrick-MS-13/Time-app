fd650f01a584f7edbc428e6f3869235886deec7b
Patrick MS <patrickms567@gmail.com>
2024-11-29 12:06:04 +0530

    Cursor componet updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import TimeManagement from './component/TimeManagement'; 
+import TimeManagement from './component/TimeManagement';
+import Cursor from './component/Cursor';
 // import './style/timeManagement.css';
 
 function App() {
   return (
     <div className="app-container">
+      <Cursor/>
       <TimeManagement /> 
     </div>
   );

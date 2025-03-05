96382dcf7b4ed127ea377b3f539d905e6852c5f0
Patrick-MS-13 <patrickms567@gmail.com>
2025-03-05 11:26:44 +0530

    updated for the delated of cursor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import TimeManagement from './component/TimeManagement';
-import Cursor from './component/Cursor';
 // import './style/timeManagement.css';
 
 function App() {
   return (
     <div className="app-container">
-      {/* <Cursor/> */}
       <TimeManagement /> 
     </div>
   );
